7549aed0c85af2020bcdb8ccd12480ec7ddad95e
Adam Wight <adam.wight@wikimedia.de>
2022-11-21 14:49:27 +0100

    Integration tests for ExternalData
    Change-Id: Ic3ae3b8f6750cc719d01dc9766b122e71e07f5c9

--- test/DataManager.test.js
+++ test/DataManager.test.js
@@ -31,7 +31,44 @@ describe( 'DataManager', function () {
 		expect( result[ 0 ].getGeoJSON() ).toStrictEqual( feature );
 	} );
 
-	test( 'failures are logged and returned', async () => {
+	test( 'happy ExternalData', async () => {
+		const feature = {
+			type: 'ExternalData',
+			service: 'geoshape',
+			url: 'http://a.test/'
+		};
+		const mapdata = { group1: feature };
+		const apiResponse = {
+			query: {
+				pages: [ {
+					mapdata: JSON.stringify( mapdata )
+				} ]
+			}
+		};
+		const mwApi = jest.fn().mockResolvedValue( apiResponse );
+		const externalResponse = {
+			type: 'Feature',
+			geometry: {
+				coordinates: [ 13, 47 ],
+				type: 'Point'
+			}
+		};
+		const getJSON = jest.fn().mockResolvedValue( externalResponse );
+		const dataManager = dataManagerLib( {
+			...wrappers,
+			mwApi,
+			getJSON
+		} );
+
+		const result = await dataManager.loadGroups( [ 'group1' ] );
+		expect( result.length ).toBe( 1 );
+		// FIXME: This is a weird and unuseful convention.
+		expect( result[ 0 ].id ).toStrictEqual( JSON.stringify( feature ) );
+		expect( result[ 0 ].isExternal ).toStrictEqual( true );
+		expect( result[ 0 ].getGeoJSON() ).toEqual( expect.objectContaining( externalResponse ) );
+	} );
+
+	test( 'failure from fetch is logged and returned', async () => {
 		const feature = [ {
 			type: 'ExternalData',
 			url: null
@@ -53,12 +90,49 @@ describe( 'DataManager', function () {
 		} );
 
 		const result = await dataManager.loadGroups( [ 'group1' ] );
-		expect( log ).toHaveBeenCalledWith( 'warn',
-			'mapdata group load failed with error Error: ExternalData has no url for group undefined' );
+
 		expect( result.length ).toBe( 2 );
 		expect( result[ 0 ].id ).toBe( 'group1' );
 		expect( result[ 0 ].isExternal ).toBe( false );
 		expect( result[ 0 ].failed ).toBe( true );
+		// FIXME: This is a deceiving, broken group.
 		expect( result[ 1 ].isExternal ).toBe( true );
+		expect( result[ 1 ].failed ).toBeUndefined();
+		expect( log ).toHaveBeenCalledWith( 'warn',
+			'mapdata group load failed with error Error: ExternalData has no url for group undefined' );
+	} );
+
+	test( 'failure from parse is logged and returned', async () => {
+		const feature = [ {
+			type: 'ExternalData',
+			service: 'foo',
+			url: 'http://a.test/'
+		} ];
+		const mapdata = { group1: feature };
+		const apiResponse = {
+			query: {
+				pages: [ {
+					mapdata: JSON.stringify( mapdata )
+				} ]
+			}
+		};
+		const log = jest.fn();
+		const mwApi = jest.fn().mockResolvedValue( apiResponse );
+		const getJSON = jest.fn().mockResolvedValue( {} );
+		const dataManager = dataManagerLib( {
+			...wrappers,
+			log,
+			mwApi,
+			getJSON
+		} );
+
+		const result = await dataManager.loadGroups( [ 'group1' ] );
+		expect( result.length ).toBe( 2 );
+		expect( result[ 0 ].failed ).toBe( true );
+		// FIXME: This is a deceiving, broken group.
+		expect( result[ 1 ].isExternal ).toBe( true );
+		expect( result[ 1 ].failed ).toBeUndefined();
+		expect( log ).toHaveBeenCalledWith( 'warn',
+			'mapdata group load failed with error Error: Unknown externalData service "foo" for group undefined' );
 	} );
 } );
